062fe62313d92617ea59b0f76da8805d710074ba
Blake Walters <blake@markupboy.com>
2014-04-14 11:08:52 -0600

    initial commit

---
+++ answers.js
@@ -0,0 +1,16 @@
+// Question 1
+
+SerializeForm = function(selector) {
+
+
+
+};
+
+
+// Question 2
+
+FlattenObject = function(object) {
+
+
+
+};
---
+++ spec/TestSpec.js
@@ -0,0 +1,36 @@
+describe("Big Cartel Front-End Dev Test", function() {
+
+  xit("serializes a form into JSON", function() {
+    expectedOutput = {
+      'input_one': 'value one',
+      'input_four': 'value four',
+      'input_five': 'value five',
+      'input_seven': 'value seven',
+      'input_nine': 'option 1'
+    };
+
+    expect(SerializeForm('#serialize_me')).toEqual(expectedOutput);
+    expect(SerializeForm('.serialize_me')).toEqual(expectedOutput);
+    expect(SerializeForm('form[name=serialize_me]')).toEqual(expectedOutput);
+    expect(SerializeForm('form[name=not_found]')).toEqual({});
+  });
+
+  it("flattens the given object", function() {
+    input = {
+      'index': {
+        'about': {
+          'team': true,
+          'company': ['Jim', 'Barry']
+        }
+      }
+    };
+
+    output = {
+      'index/about/team': true,
+      'index/about/company': ['Jim', 'Barry']
+    };
+
+    expect(FlattenObject(input)).toEqual(output);
+  });
+
+});
